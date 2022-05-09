ebfb8bf03fdb6d3d14f1f97b7dbdffa40e22ea60
KomaCrazy <96609349+KomaCrazy@users.noreply.github.com>
2022-05-09 13:45:42 +0700

    Update search.py

--- day3/search.py
+++ day3/search.py
@@ -1,12 +1,17 @@
 import sqlite3
+count = 0
 def search():
+  global count 
   try:
       with sqlite3.connect("data1.sqlite") as con :   
         cmd = """
         select * from box1;
         """
-      con.execute(cmd)
-      print(con.execute(cmd))
+      for row in con.execute(cmd):
+          count = count + 1
+          print(row)
+      
   except Exception as e :
       print("Error :".format(e))
 search()
+print("Profile : ",count)
