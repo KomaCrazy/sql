cffd4feabfe7959002eaf50a61f65ffdae10e710
KomaCrazy <96609349+KomaCrazy@users.noreply.github.com>
2022-05-11 09:11:17 +0700

    1
    1

--- day7/app.py
+++ day7/app.py
@@ -2,6 +2,7 @@ import sqlite3
 
 path = "data.sqlite"
 table = "data"
+as_34 = chr(34)
 user = ""
 
 
@@ -26,19 +27,29 @@ def show_data():
     except Exception as e:
         print("Error : show data")
     
-
+def search():
+    try:
+        user = str(input("user : "))
+        print("==============================")
+        with sqlite3.connect(path) as conn:
+            sql_cmd = "select * from " + table + " where user=" + as_34 + user + as_34 + ";"
+            for row in conn.execute(sql_cmd) :
+                print(row)
+    except Exception as e :
+        print("Error : search")
 def cmd_fun(cmd):
     if cmd == "1":
         create_table()
     elif cmd == "2":
         show_data()
+    elif cmd == "3" :
+        search()
 
 
 while True:
-    print("=======================")
     print("Enter : 1 create_table")
     print("Enter : 2 showdata")
     print("Enter : 3 serach ")
     cmd = str(input("Enter : "))
-    cmd_fun(cmd)
     print("=======================")
+    cmd_fun(cmd)
