73dfef2548e81e2ea4070c74fce9702d58ff6b47
KomaCrazy <96609349+KomaCrazy@users.noreply.github.com>
2022-05-11 09:54:18 +0700

    1
    1

--- day7/server.py
+++ day7/server.py
@@ -2,6 +2,7 @@ import sqlite3
 import json
 from flask import Flask , jsonify
 from flask_cors import CORS
+from requests import patch
 
 app = Flask(__name__)
 CORS(app)
@@ -13,9 +14,29 @@ link_0 = '/'
 link_1 = '/1'
 link_2 = '/2'
 
+path = "data.sqlite"
+
 @app.route(link_0)
 def link0():
     return "hello world"
+
+@app.route(link_1)
+def link1():
+    try:
+        with sqlite3.connect(path) as conn :
+          sql_cmd = "select * from data;"
+          data = []
+          for row in conn.execute(sql_cmd):
+              table = {}
+              table["id"] = row[0]
+              table["user"] = row[1]
+              table["password"] = row[2]
+              table["age"] = row[3]
+              table["contact"] = row[4]
+              data.append(table)
+        return jsonify(data)
+    except Exception as e :
+        print("Error : "+link_1)
 
 
 if __name__ == '__main__':
---
+++ day7/test.py
@@ -0,0 +1,3 @@
+link1 = "/"
+link1 = link1.encode()
+print(link1)
