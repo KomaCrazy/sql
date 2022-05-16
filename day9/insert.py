672fb52d97a8a32a78c8f6c60fe385174ecc88d8
KomaCrazy <96609349+KomaCrazy@users.noreply.github.com>
2022-05-16 09:53:05 +0700

    1
    1

--- day9/insert.py
+++ day9/insert.py
@@ -1,18 +1,25 @@
 from re import A
 import sqlite3
 from tools import *
+
+
 def show():
+    global id , key
     try:
+        id = 0
         with sqlite3.connect("data.sqlite") as conn :
-            id = 0
             for row in conn.execute(sql_select) :
-                print(row)
-                id = id + 1
+                if key == 1 :
+                    print(row)
+                
+                id = id  + 1
+            key = 0 
     except Exception as e:
         print("Error show")
 
 
 def insert():
+    global id ,key , sql_insert
     try:
         with sqlite3.connect("data.sqlite") as conn :  
             
@@ -20,13 +27,16 @@ def insert():
             p = str(input("Password : "))
             a = str(input("age :"))
             show()
+            key = key + 1
             u = as_34 + u + as_34
             p = as_34 + p + as_34
             a = as_34 + a + as_34
-            print(id)
-            print(u)
-            print(a)
-            print(p)
+            sql_insert = sql_insert + str(id+1) + "," + u + "," + p + "," + a + ");"
+            conn.execute(sql_insert)
+            
     except Exception as e:
         print("Error : insert")
 insert()
+print("______________________")
+show()
+key = 0 
--- day9/tools.py
+++ day9/tools.py
@@ -1,5 +1,6 @@
 table = "data"
 as_34 = chr(34)
+key = 0
 id = 0
 sql_cmd_1 = "(id primary key, user text, password text, age text);"
 sql_cmd = "create table " + table +sql_cmd_1
